faf908c09025fb6f88bf2786ef60c4f036612a93
iftakhar <iftakharalamshuvo11@gmail.com>
2024-01-28 11:18:39 +0600

    task 1 added

---
+++ task1.js
@@ -0,0 +1,12 @@
+// 1. Declare an array 
+// 1. Declare an array with 5 elements containing fruits
+// 2. console log the 3rd index element
+// 3. change the value of the 2nd index element to jambura
+// 4. console log the final array
+ 
+const fruits =['mango', 'apple', 'banana', 'guava', 'sopeda'];
+console.log(fruits[3]);    //3rd index element k console log kora holo 
+
+fruits[2]='jambura';      //2nd index element e new element add kora holo new element add korle ager element ta cole gyea tar jagai new element ase ekhane banna er jaigai jambura aslo
+console.log(fruits);
+
