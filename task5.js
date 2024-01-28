ad3683ca36b1d921b51c2e604e531b867b9bf517
iftakhar <iftakharalamshuvo11@gmail.com>
2024-01-28 11:48:17 +0600

    task 5 added

---
+++ task5.js
@@ -0,0 +1,16 @@
+// ### 5. Combining Arrays
+
+// Instructions:
+
+// 1. Create two arrays of your choice.
+// 2. Use the concat method to combine the two arrays into a new array.
+// 3. Print both the original arrays and the combined array using console.log().
+const books=['Accounting', 'management', 'alaler gorer dulal', 'pother pasaali', 'biral', 'javascript'];
+const fruits =['mango', 'apple', 'banana', 'guava', 'sopeda'];
+const combained=books.concat(fruits)
+
+console.log(books);
+console.log(fruits);
+console.log(combained);
+
+// concat method use kore amra 2 ta array k aksathe akt array er moddhe dekhate pari .tobe mul array ta change hoi na concat korle ,amra concat kore r o akta variable er moddhe rakhte pari.
