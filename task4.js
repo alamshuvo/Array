f493b8c562eb437b617ae63c3f2b3f994e88f304
iftakhar <iftakharalamshuvo11@gmail.com>
2024-01-28 11:43:11 +0600

    task 4 added

---
+++ task4.js
@@ -0,0 +1,21 @@
+
+// ### 4. Checking if it's an Array
+
+// Instructions:
+
+// 1. Create different variables, each containing either an array or a non-array value.
+
+// 2. Now use isArray to check if each variable is an array.
+
+// 3. Print a message to the console indicating whether each variable is an array or not.
+const books=['Accounting', 'management', 'alaler gorer dulal', 'pother pasaali', 'biral', 'javascript'];
+const book='paithon';
+const fruits=['apple', 'bananna', 'jambura', 'graps'];
+const fruit='orange';
+
+console.log(Array.isArray(books));
+console.log(Array.isArray(book));
+console.log(Array.isArray(fruits));
+console.log(Array.isArray(fruit));
+
+// isArray ei method er maddome amra sadaronoto ekta variable array kina ota check korte pari jodi array hoi tahole true value return korbe and jodi array na hoi tahole false value return korbe .
